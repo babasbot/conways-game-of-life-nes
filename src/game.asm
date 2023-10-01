907c2dfb3353f0fb755949a2693564aafc4952cc
Luis Alfredo Lorenzo <babasbot@gmail.com>
2023-10-01 15:11:13 -0600

    Removes dead includes

--- src/game.asm
+++ src/game.asm
@@ -1,7 +1,5 @@
 .include "./constants.inc"
 
 .include "./segments/zeropage.inc"
-.include "./segments/code.inc"
-.include "./segments/vectors.inc"
 .include "./segments/chr.inc"
 .include "./segments/rodata.inc"
