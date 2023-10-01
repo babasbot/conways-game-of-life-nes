8e65956ab19f6bf6e4a080276c2f2a67c80ab43d
Luis Alfredo Lorenzo <babasbot@gmail.com>
2023-10-01 15:12:51 -0600

    Defines the `CHR` segment

---
+++ src/chr.asm
@@ -0,0 +1,4 @@
+.segment "CHR"
+
+.incbin "graphics.chr"
+
--- src/game.asm
+++ src/game.asm
@@ -1,5 +1,4 @@
 .include "./constants.inc"
 
 .include "./segments/zeropage.inc"
-.include "./segments/chr.inc"
 .include "./segments/rodata.inc"
