245c78539fbccf97afc3213f84ef06a0b9010825
Luis Alfredo Lorenzo <babasbot@gmail.com>
2023-10-01 16:40:37 -0600

    Updates PPU_MASK and PPU_SCROLL values in the nmi_hanlder

--- src/nmi_hanlder.asm
+++ src/nmi_hanlder.asm
@@ -12,6 +12,26 @@
 .proc nmi_handler
   OAM_DMA_TRANSFER
 
+  lda #$00
+  sta PPU_SCROLL
+  sta PPU_SCROLL
+
+  lda #%00011110
+  sta PPU_MASK
+
+  ; 7654 3210  PPU MASK
+  ; ---- ----
+  ; BGRs bMmG  See: https://www.nesdev.org/wiki/PPU_registers#PPUMASK
+  ; |||| ||||
+  ; |||| |||+- Greyscale (0: normal color, 1: produce a greyscale display)
+  ; |||| ||+-- 1: Show background in leftmost 8 pixels of screen, 0: Hide
+  ; |||| |+--- 1: Show sprites in leftmost 8 pixels of screen, 0: Hide
+  ; |||| +---- 1: Show background
+  ; |||+------ 1: Show sprites
+  ; ||+------- Emphasize red
+  ; |+-------- Emphasize green
+  ; +--------- Emphasize blue
+
   rti
 .endproc
 
