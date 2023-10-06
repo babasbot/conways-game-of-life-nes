a3cd3f10a1a1c456c17716130c0f094961e2617d
Luis Alfredo Lorenzo <babasbot@gmail.com>
2023-10-06 16:51:48 -0600

    Updates the game graphics

--- src/rodata.asm
+++ src/rodata.asm
@@ -1,15 +1,15 @@
 .segment "RODATA"
 
 ppu_palettes:
-  .byte $0d, $30, $0d, $0d ; background palette 0
+  .byte $0d, $30, $2a, $0d ; background palette 0
   .byte $0d, $30, $0d, $0d ; background palette 1
   .byte $0d, $30, $0d, $0d ; background palette 2
   .byte $0d, $30, $0d, $0d ; background palette 3
 
-  .byte $0d, $30, $0d, $0d ; sprite palette 0
-  .byte $0d, $30, $0d, $0d ; sprite palette 1
-  .byte $0d, $30, $0d, $0d ; sprite palette 2
-  .byte $0d, $30, $0d, $0d ; sprite palette 3
+  .byte $0d, $0d, $0d, $0d ; sprite palette 0
+  .byte $0d, $0d, $0d, $0d ; sprite palette 1
+  .byte $0d, $0d, $0d, $0d ; sprite palette 2
+  .byte $0d, $0d, $0d, $0d ; sprite palette 3
 
 .export ppu_palettes
 
